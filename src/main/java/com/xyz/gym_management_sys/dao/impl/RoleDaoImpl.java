4f0421ccabdbcae7aa2bec8444451cd58e750746
HankXu <527665221@qq.com>
2017-05-24 16:10:58 +0800

    编写Role的DAO类

---
+++ src/main/java/com/xyz/gym_management_sys/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,38 @@
+package com.xyz.gym_management_sys.dao.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.xyz.gym_management_sys.dao.RoleDao;
+import com.xyz.gym_management_sys.po.Role;
+
+@Repository
+public class RoleDaoImpl implements RoleDao {
+
+	@Resource
+	private HibernateTemplate hibernate;
+	
+	public void addRole(Role role) {
+		hibernate.save(role);
+	}
+
+	public void updateRole(Role role) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteRole(Role role) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Role> findRoleByNotNullProp(Role role) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
